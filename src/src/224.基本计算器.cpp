c225b7caa6df66ee0ba6d8a98f694e4d8a86262c
Neojan <neojanlocke@gmail.com>
2023-04-10 20:29:30 +0800

    Update 224.基本计算器.cpp

--- src/src/224.基本计算器.cpp
+++ src/src/224.基本计算器.cpp
@@ -58,3 +58,62 @@ public:
 };
 // @lc code=end
 
+#if 0
+class Solution
+{
+public:
+    int calculate(string s)
+    {
+        int sum = 0;
+        int temp = 0;
+        int pre = 0;
+        char sign = '+';
+        stack<int> nums;
+        for (int i = 0; i < s.length(); i++)
+        {
+            if (isdigit(s[i]))
+                temp = (temp * 10 + (s[i] - '0'));
+
+            // if (s[i] == '(')
+            // {
+            //     temp = calculate(s.substr(i+1));
+            // }
+            if (!isdigit(s[i]) && s[i] != ' ' || i == s.length() - 1)
+            {
+                switch (sign)
+                {
+                case '+':
+                    nums.push(temp); break;
+                case '-':
+                    nums.push(-temp); break;
+                case '*':
+                    pre = nums.top();
+                    nums.pop();
+                    nums.push(pre * temp);
+                    break;
+                case '/':
+                    pre = nums.top();
+                    nums.pop();
+                    nums.push(pre / temp);
+                    break;
+                default:
+                    break;
+                }
+
+                temp = 0;
+                sign = s[i];
+            }
+            // if (s[i] == ')')
+            // {
+            //     break;
+            // }
+        }
+        while (!nums.empty())
+        {
+            sum += nums.top();
+            nums.pop();
+        }
+        return sum;
+    }
+};
+#endif
